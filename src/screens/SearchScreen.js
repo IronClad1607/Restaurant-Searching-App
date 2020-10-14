9072d513513a185af99462961412a542380409e4
Ishaan Jain <ishaan160799@gmail.com>
2020-10-14 20:21:08 +0530

    useEffect for first render only added

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
 import yelp from "../api/yelp";
@@ -8,28 +8,37 @@ const SearchScreen = () => {
   const [results, setResults] = useState([]);
   const [error, setError] = useState("");
 
-  const searchApi = async () => {
+  console.log(`search term: ${searchTerm}`);
+  const searchApi = async (term) => {
+    console.log("Hi There");
     try {
       const response = await yelp.get("/search", {
         params: {
           limit: 50,
-          term: searchTerm,
+          term: term,
           location: "san jose",
         },
       });
       setResults(response.data.businesses);
-      setError('');
+      setError("");
     } catch (err) {
       setError("Something went wrong");
     }
   };
+
+  //   searchApi('pasta');
+
+  useEffect(() => {
+      console.log("first time")
+    searchApi("pasta");
+  }, []);
 
   return (
     <View>
       <SearchBar
         term={searchTerm}
         onSearchChange={(newTerm) => setSearchTerm(newTerm)}
-        onTermSubmit={() => searchApi()}
+        onTermSubmit={() => searchApi(searchTerm)}
       />
       <Text>We have found {results.length} results</Text>
       {error ? <Text>{error}</Text> : null}
